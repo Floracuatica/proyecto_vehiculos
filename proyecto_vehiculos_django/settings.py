7b87dfd7b6301c5a40acd2fef73ab502c738ea7e
Floracuatica <rsbejar@gmail.com>
2024-12-03 03:57:50 -0300

    Update settings.py

--- proyecto_vehiculos_django/settings.py
+++ proyecto_vehiculos_django/settings.py
@@ -7,11 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'clave-por-defecto')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
-# Allowed hosts for production and local development
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,proyecto-vehiculos.onrender.com').split(',')
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
