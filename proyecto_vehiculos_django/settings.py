b3dd7c0740097d419cd2389ba5c24e0b18359212
Floracuatica <rsbejar@gmail.com>
2024-12-03 21:28:35 -0300

    Update settings.py

--- proyecto_vehiculos_django/settings.py
+++ proyecto_vehiculos_django/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'clave-por-defecto')
 
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = 'True'
 ALLOWED_HOSTS = ['proyectovehiculos-a1790bd1e6af.herokuapp.com', '127.0.0.1', 'localhost']
 
 
